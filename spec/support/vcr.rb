6d85827a5287652437ea43ee74dd5938e55adb50
Andrea Schiavini <metalelf0@gmail.com>
2015-05-31 13:34:06 +0200

    Filter data from cassettes

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -3,4 +3,10 @@ VCR.configure do |c|
   # Faraday 0.9 bug: https://github.com/vcr/vcr/issues/386
   c.hook_into :webmock
   c.configure_rspec_metadata!
+  c.filter_sensitive_data('<TWITTER_SECRET>') do
+    ENV['TWITTER_SECRET_KEY']
+  end
+  c.filter_sensitive_data('<TWITTER_CONSUMER>') do
+    ENV['TWITTER_CONSUMER_KEY']
+  end
 end
