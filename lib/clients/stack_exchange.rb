1e9ba30711ac430662fb50e4df2d7615c8453e5d
Andrea Schiavini <metalelf0@gmail.com>
2015-05-31 09:47:53 +0200

    Hiding connection inside StackExchange client too

--- lib/clients/stack_exchange.rb
+++ lib/clients/stack_exchange.rb
@@ -2,9 +2,12 @@ class Clients::StackExchange
 
   BASE_URL         = 'https://api.stackexchange.com'
 
-  def fetch_user(user_name, connection=nil)
-    connection ||= Faraday.new(url: BASE_URL)
-    response = connection.get(users_path(user_name, 'stackoverflow'))
+  def self.connection
+    @@connection ||= Faraday.new(url: BASE_URL)
+  end
+
+  def fetch_user(user_name)
+    response = self.class.connection.get(users_path(user_name, 'stackoverflow'))
     users = JSON.parse(response.body)
     users['items'].select { |user| user['display_name'] == user_name }.first
   end
