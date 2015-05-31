d8c7a039c83696716d85c7e6fe685e7e9e4fea1d
Andrea Schiavini <metalelf0@gmail.com>
2015-05-31 08:56:17 +0200

    Refactor

--- lib/clients/hacker_news.rb
+++ lib/clients/hacker_news.rb
@@ -4,18 +4,20 @@ class Clients::HackerNews
   TOP_STORIES_PATH = '/v0/topstories.json?print=pretty'
 
   def top_stories(limit=50)
-    begin
-      conn = Faraday.new(:url => BASE_URL)
-      response = conn.get TOP_STORIES_PATH
-      ids = JSON.parse(response.body).take(limit)
-      data = [] 
-      ids.each do |id|
-        data << JSON.parse(conn.get(story_path(id)).body)
-      end
-      return data
-    rescue
-      return [] 
+    conn = Faraday.new(url: BASE_URL)
+    response = conn.get TOP_STORIES_PATH
+    top_story_ids = JSON.parse(response.body).take(limit)
+    data = [] 
+    top_story_ids.each do |story_id|
+      data << fetch_story(story_id, conn)
     end
+    return data
+  end
+
+  def fetch_story(story_id, connection=nil)
+    connection ||= Faraday.new(url: base_url)
+    response = connection.get(story_path(story_id))
+    return JSON.parse(response.body)
   end
 
   private
